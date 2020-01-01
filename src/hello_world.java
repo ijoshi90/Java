146208216111ae6eaa12707cd609866731b2bf5a
Akshay Joshi <akshay.joshi.m@gmail.com>
2020-01-01 16:57:16 +0100

    Initial commit

---
+++ src/hello_world.java
@@ -0,0 +1,12 @@
+/*
+Author : Akshay Joshi
+GitHub : https://github.com/ijoshi90
+Created on 01-01-2020 at 16:53
+*/
+
+public class hello_world {
+    public static void main(String args[]) {
+        System.out.println("Hello World!");
+    }
+
+}

26955dc645a566ee1d216496366bade451977091
Akshay Joshi <akshay.joshi.m@gmail.com>
2020-01-01 17:38:50 +0100

    Update hello_world.java

--- src/hello_world.java
+++ src/hello_world.java
@@ -4,6 +4,8 @@ GitHub : https://github.com/ijoshi90
 Created on 01-01-2020 at 16:53
 */
 
+export CLASSPATH = ${CLASSPATH}:.
+
 public class hello_world {
     public static void main(String args[]) {
         System.out.println("Hello World!");
